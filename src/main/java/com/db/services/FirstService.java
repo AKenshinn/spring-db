0124a532bd6fc094e08be154a76deee166b936f8
Anat <anat.pantera@gmail.com>
2015-10-04 21:18:42 +0700

    add service, test insert service pass

---
+++ src/main/java/com/db/services/FirstService.java
@@ -0,0 +1,10 @@
+package com.db.services;
+
+import java.io.Serializable;
+
+import com.db.entities.First;
+import com.db.services.base.GenericService;
+
+public interface FirstService extends GenericService<First, Serializable> {
+
+}

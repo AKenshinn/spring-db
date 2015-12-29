039fd5e5b36f88989ce40dd63e448ccab1b449b5
Anat <anat.pantera@gmail.com>
2015-12-29 21:04:10 +0700

    remove First(Test entity,repository,service)

--- src/main/resources/scripts/init-data.sql
+++ src/main/resources/scripts/init-data.sql
@@ -1,5 +1,1 @@
-INSERT INTO COURSES (NAME, CREATED_DATE_TIME, UPDATED_DATE_TIME) VALUES
-('English', NOW(), NOW()),
-('Math', NOW(), NOW()),
-('Physics', NOW(), NOW()),
-('Chemical', NOW(), NOW());
+

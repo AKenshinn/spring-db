3a14144d3229ad8bb258952cb8ac9acbbd38b9af
Anat Abdullagasim <anat.pantera@gmail.com>
2017-03-11 16:08:51 +0700

    Add new method for test Jenkins

--- src/test/java/com/db/repositories/StudentRepositoryTest.java
+++ src/test/java/com/db/repositories/StudentRepositoryTest.java
@@ -21,96 +21,101 @@ import java.util.List;
  * Created by Kenshinn on 12/29/2015 AD.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:/applicationContext.xml"})
+@ContextConfiguration(locations = {"classpath:/applicationContext.xml"})
 @TransactionConfiguration
 @Transactional
 public class StudentRepositoryTest {
 
-  @Autowired
-  private StudentRepository studentRepository;
+    @Autowired
+    private StudentRepository studentRepository;
 
-  private Student anat, akkhawat;
+    private Student anat, akkhawat;
 
-  @Before
-  public void setUp() throws Exception {
-    anat = new Student();
-    anat.setFirstName("Anat");
-    anat.setLastName("Abdullagasim");
-    studentRepository.insert(anat);
+    @Before
+    public void setUp() throws Exception {
+        anat = new Student();
+        anat.setFirstName("Anat");
+        anat.setLastName("Abdullagasim");
+        studentRepository.insert(anat);
 
-    akkhawat = new Student();
-    akkhawat.setFirstName("Akkhawat");
-    akkhawat.setLastName("Dilokchalermkul");
-    studentRepository.insert(akkhawat);
+        akkhawat = new Student();
+        akkhawat.setFirstName("Akkhawat");
+        akkhawat.setLastName("Dilokchalermkul");
+        studentRepository.insert(akkhawat);
 
-  }
+    }
 
-  @Test
-  public void testLoadStudentRepositoryShouldPass() throws Exception {
-    assertNotNull(studentRepository);
-  }
+    @Test
+    public void testLoadStudentRepositoryShouldPass() throws Exception {
+        assertNotNull(studentRepository);
+    }
 
-  @Test
-  public void testFindByIdWithStudentRepositoryShouldReturnStudentThatSetup() throws Exception {
-    Student result = studentRepository.findById(anat.getId());
-    assertNotNull(result);
-    assertThat(result.getId(), is(anat.getId()));
-    assertThat(result.getFirstName(), is("Anat"));
-    assertThat(result.getLastName(), is("Abdullagasim"));
+    @Test
+    public void testFindByIdWithStudentRepositoryShouldReturnStudentThatSetup() throws Exception {
+        Student result = studentRepository.findById(anat.getId());
+        assertNotNull(result);
+        assertThat(result.getId(), is(anat.getId()));
+        assertThat(result.getFirstName(), is("Anat"));
+        assertThat(result.getLastName(), is("Abdullagasim"));
 
-  }
+    }
 
-  @Test
-  public void testFindAllWithStudentRepositoryShouldReturnListOfAllStudents() throws Exception {
-    List<Student> result = studentRepository.findAll();
-    assertNotNull(result);
-    assertThat(result.size(), is(new GreaterOrEqual<>(2)));
+    @Test
+    public void testFindAllWithStudentRepositoryShouldReturnListOfAllStudents() throws Exception {
+        List<Student> result = studentRepository.findAll();
+        assertNotNull(result);
+        assertThat(result.size(), is(new GreaterOrEqual<>(2)));
 
-  }
+    }
 
-  @Test
-  public void testUpdateWithStudentRepositoryShouldChangeAnatToHimura() throws Exception {
-    Student update = studentRepository.findById(anat.getId());
-    update.setFirstName("Himura");
-    studentRepository.update(update);
+    @Test
+    public void testUpdateWithStudentRepositoryShouldChangeAnatToHimura() throws Exception {
+        Student update = studentRepository.findById(anat.getId());
+        update.setFirstName("Himura");
+        studentRepository.update(update);
 
-    Student result = studentRepository.findById(update.getId());
-    assertThat(result.getId(), is(anat.getId()));
-    assertThat(result.getFirstName(), is("Himura"));
+        Student result = studentRepository.findById(update.getId());
+        assertThat(result.getId(), is(anat.getId()));
+        assertThat(result.getFirstName(), is("Himura"));
 
-  }
+    }
 
-  @Test
-  public void testDeleteWithStudentRepositoryShouldNotFindAnat() throws Exception {
-    Student delete = studentRepository.findById(anat.getId());
-    studentRepository.delete(delete);
+    @Test
+    public void testDeleteWithStudentRepositoryShouldNotFindAnat() throws Exception {
+        Student delete = studentRepository.findById(anat.getId());
+        studentRepository.delete(delete);
 
-    Student result = studentRepository.findById(delete.getId());
-    assertNull(result);
+        Student result = studentRepository.findById(delete.getId());
+        assertNull(result);
 
-  }
+    }
 
-  @Test
-  public void testDeleteByIdWithStudentRepositoryShouldNotFindAkkhawat() throws Exception {
-    Student delete = studentRepository.findById(akkhawat.getId());
-    studentRepository.deleteById(delete.getId());
+    @Test
+    public void testDeleteByIdWithStudentRepositoryShouldNotFindAkkhawat() throws Exception {
+        Student delete = studentRepository.findById(akkhawat.getId());
+        studentRepository.deleteById(delete.getId());
 
-    Student result = studentRepository.findById(delete.getId());
-    assertNull(result);
+        Student result = studentRepository.findById(delete.getId());
+        assertNull(result);
 
-  }
+    }
 
-  @Test
-  public void testSearchWithStudentRepositoryShouldFindAnat() throws Exception {
-    HashMap<String, Object> parameterMap = new HashMap<>();
-    parameterMap.put("firstName", anat.getFirstName());
-    parameterMap.put("lastName", anat.getLastName());
+    @Test
+    public void testSearchWithStudentRepositoryShouldFindAnat() throws Exception {
+        HashMap<String, Object> parameterMap = new HashMap<>();
+        parameterMap.put("firstName", anat.getFirstName());
+        parameterMap.put("lastName", anat.getLastName());
 
-    List<Student> result = studentRepository.search(parameterMap);
-    assertNotNull(result);
-    assertThat(result.get(0).getFirstName(), is("Anat"));
-    assertThat(result.get(0).getLastName(), is("Abdullagasim"));
+        List<Student> result = studentRepository.search(parameterMap);
+        assertNotNull(result);
+        assertThat(result.get(0).getFirstName(), is("Anat"));
+        assertThat(result.get(0).getLastName(), is("Abdullagasim"));
 
-  }
+    }
+    
+    @Test
+    public void newMethodForTestJenkins() throws Exception {
+        assertTrue(true);
+    }
 
 }
